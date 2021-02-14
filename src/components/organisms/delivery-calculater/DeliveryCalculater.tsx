6d9c48d3dbf411c6cd60b87ef884d8b6bee70aa1
topvitayaje <top@7peakssoftware.com>
2021-02-14 21:09:48 +0700

    feat(delivery-calculater): add DeliveryCalculater component

---
+++ src/components/organisms/delivery-calculater/DeliveryCalculater.tsx
@@ -0,0 +1,13 @@
+import RouteCostCalculater from '../../molecules/route-cost-calculater'
+
+type DeliveryCalculaterProps = {}
+const DeliveryCalculater: React.FC<DeliveryCalculaterProps> = () => {
+  return (
+    <div>
+      {/* TODO: create tabs */}
+      <RouteCostCalculater />
+    </div>
+  )
+}
+
+export default DeliveryCalculater
