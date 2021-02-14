5f7031b1cb0209e3cce9f1b0ed7007ee0a9b0bb1
topvitayaje <top@7peakssoftware.com>
2021-02-14 21:10:14 +0700

    feat(delivery-calculater): add container for DeleiveryCalculator component

---
+++ src/containers/delivery-calculater/DeliveryCalculatorContainer.tsx
@@ -0,0 +1,13 @@
+import DeliveryCalculater from '../../components/organisms/delivery-calculater'
+
+type DeliveryCalculatorContainerProps = {}
+
+const DeliveryCalculatorContainer: React.FC<DeliveryCalculatorContainerProps> = () => {
+  return (
+    <div>
+      <DeliveryCalculater />
+    </div>
+  )
+}
+
+export default DeliveryCalculatorContainer
