d88d783713f73f6858c9ae3228d0627667aef14c
topvitayaje <top@7peakssoftware.com>
2021-02-14 21:00:03 +0700

    chore: clean up App files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Main from './containers/main'
+import RouteCalculater from './containers/route-calculator'
+import CommonLayout from './layouts/common/CommonLayout'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <CommonLayout>
+      <h1>Delivery Service</h1>
+      <RouteCalculater />
+      {/* TODO: remove main later */}
+      <Main />
+    </CommonLayout>
+  )
 }
 
-export default App;
+export default App
